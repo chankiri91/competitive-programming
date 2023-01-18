a634cec97d57b7b4d21b540a63fe9f7c304b2a71
kiri <81542048+kiri-91@users.noreply.github.com>
2023-01-18 23:38:11 +0900

    ナップザック問題

--- src/main/kotlin/algo_math/030.kt
+++ src/main/kotlin/algo_math/030.kt
@@ -1,5 +1,13 @@
 package algo_math
 
 fun main() {
-
+    val (N, W) = readLine()!!.split(" ").map { it.toInt() }
+    val dp = LongArray(W + 1)
+    repeat(N) {
+        val (wi, vi) = readLine()!!.split(" ").map { it.toInt() }
+        for (i in W - wi downTo 0) {
+            if (dp[i + wi] < dp[i] + vi) dp[i + wi] = dp[i] + vi
+        }
+    }
+    println(dp[W])
 }
